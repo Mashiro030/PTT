bfcd14526ec687c5b5f3955e7576cb4bf2259578
邵柏鈞 <46882597+Mashiro030@users.noreply.github.com>
2019-05-31 20:36:34 +0800

    爬到文章連結的下一層

--- beautifulsoup完整版.py
+++ beautifulsoup完整版.py
@@ -12,7 +12,8 @@ def get_all_href(url):
         there_is_a_item = item.select_one("a")
         title = item.text
         if there_is_a_item:#確認文章有沒有被刪除
-            print(title, domain + there_is_a_item.get('href'))
+            article_url=there_is_a_item.get('href')
+            print(title, domain + article_url)
         
 for page in range(1,4):#抓取前3頁的路徑 例如:https://www.ptt.cc/bbs/PC_Shopping/index4402.html....
     r = requests.get(url)
@@ -22,6 +23,30 @@ for page in range(1,4):#抓取前3頁的路徑 例如:https://www.ptt.cc/bbs/PC_
     next_page_url = domain + up_page_href
     url = next_page_url
     get_all_href(url = url)#呼叫函式
+
+def get_article_info(article_url):
+    response = requests.get(article_url)
+    soup = BeautifulSoup(response.text, "lxml")
+    results = soup.select('span.article-meta-value')
+    if results:
+        print('作者:', results[0].text)
+        print('看板:', results[1].text)
+        print('標題:', results[2].text)
+        print('時間:', results[3].text)
+        
+for page in range(1,4):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "lxml")
+    results = soup.select('div.title')
+    for item in results:#抓取頁面每一篇文章的連結
+        there_is_a_item = item.select_one("a")
+        title = item.text
+        if there_is_a_item:#確認文章有沒有被刪除
+            article_url=there_is_a_item.get('href')
+            get_article_info(article_url=domain+there_is_a_item.get('href'))
+
+
+
 
 
 
